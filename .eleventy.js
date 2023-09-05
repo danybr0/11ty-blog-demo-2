94ff102c436952ba9a7ed012bf6816680270c0b4
Dan Covic <dan.covic@outlook.com>
2023-09-05 14:59:38 +0200

    fixed-admin-panel

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     // fixing date formatting
     // source: https://11ty.rocks/eleventyjs/dates/
